260f9af271d8a205ab98510ea60386d361bf9267
unai <upastor@hiberus.com>
2026-03-10 13:22:14 +0100

    Some components have been added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importación de los componentes generados
+import { Home } from './features/public/home/home';
+import { Login } from './features/auth/login/login';
+import { Register } from './features/auth/register/register';
+import { Dashboard } from './features/admin/dashboard/dashboard';
+
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: Home,
+    title: 'Inicio - TravelAgency'
+  },
+  { 
+    path: 'trips', 
+    component: Home, 
+    title: 'Catálogo - TravelAgency'
+  },
+  { 
+    path: 'login', 
+    component: Login,
+    title: 'Iniciar Sesión - TravelAgency'
+  },
+  { 
+    path: 'register', 
+    component: Register,
+    title: 'Registro - TravelAgency'
+  },
+  { 
+    path: 'admin', 
+    component: Dashboard,
+    title: 'Panel de Administración'
+  },
+  
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
